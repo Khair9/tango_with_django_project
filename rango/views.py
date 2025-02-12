b1ca4dd22deaf714005f17afd0280917455d37d7
“Hanif <2906978@student.gla.ac.uk>
2025-02-12 23:34:57 +0000

    chapter 3.5 11:34pm wed

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("<a href='/rango/about/'>About</a>")
 # Create your views here.
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>.Rango says here is the about page.</a>")
